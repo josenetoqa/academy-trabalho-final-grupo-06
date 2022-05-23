ec471396727e62d28ff6271d25bc0c374506e54c
José <joseneto.2762@aluno.una.br>
2022-05-23 17:22:58 -0300

    steps definition folder

---
+++ cypress/support/steps/historico.step.js
@@ -0,0 +1,64 @@
+
+Given(/^acessei a pagina$/, () => {
+	return true;
+});
+
+When(/^digitei o email para acesso$/, () => {
+	return true;
+});
+
+When(/^digitei a senha para acesso$/, () => {
+	return true;
+});
+
+Then(/^Clico em Entrar para conseguir acesso$/, () => {
+	return true;
+});
+
+When(/^for carregado as listas$/, () => {
+	return true;
+});
+
+Then(/^clico no Historico para acessar a pagina$/, () => {
+	return true;
+});
+
+When(/^for carregado minhas listas$/, () => {
+	return true;
+});
+
+Then(/^verifico o tamanho da lista carregada$/, () => {
+	return true;
+});
+
+Then(/^clico no Historico para acessar a pagina$/, () => {
+	return true;
+});
+
+When(/^for carregado minhas listas$/, () => {
+	return true;
+});
+
+Then(/^verifico se o nome e data de criação são apresentados$/, () => {
+	return true;
+});
+
+Then(/^clico no Historico para acessar a pagina$/, () => {
+	return true;
+});
+
+When(/^for carregado minhas listas$/, () => {
+	return true;
+});
+
+Then(/^clico em uma lista$/, () => {
+	return true;
+});
+
+When(/^a lista for carregada$/, () => {
+	return true;
+});
+
+Then(/^verifico se foi apresentado o nome e item da lista$/, () => {
+	return true;
+});
