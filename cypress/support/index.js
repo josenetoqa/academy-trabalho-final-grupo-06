1f8ef4da892c65ea0227eeb615d56e5d79d14633
José <joseneto.2762@aluno.una.br>
2022-05-25 23:35:54 -0300

    implementação de configurações para BDD+CYPRESS

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,8 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+const cucumber = require('cypress-cucumber-preprocessor').default
+
+module.exports = (on, config) => {
+  on('file:preprocessor', cucumber())
+}
