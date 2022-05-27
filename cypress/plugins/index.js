37550f3dca91c0c5ef5c2fa72d8f7c183ba5ffd4
José <joseneto.2762@aluno.una.br>
2022-05-27 16:12:10 -0300

    fix

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,9 +14,15 @@
 
 /**
  * @type {Cypress.PluginConfig}
+
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+}
+const cucumber = require('cypress-cucumber-preprocessor').default
+
+module.exports = (on, config) => {
+  on('file:preprocessor', cucumber());
 }
