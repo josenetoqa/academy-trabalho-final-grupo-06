75758c28de34ddf1d0107f9f7d7e62ca6ea555b3
Guilherme Santos <guilhermeafsantos@gmail.com>
2022-05-26 23:59:53 -0300

    Given Ok

--- cypress/support/pages/perfilPage.po.js
+++ cypress/support/pages/perfilPage.po.js
@@ -1,25 +1,44 @@
+//Fazer Login
 class newLogin{
 
     //elementos
     botãoEntrar = '.sc-ftvSup'
     emailLogin = 'input[name=email]'
     senhaLogin = 'input[name=password]'
-    
+       
     //Ações 
     telaLogin(){
         cy.visit("https://academy-lembra-compras.herokuapp.com/");
     }
     preencherEmail(email){
-        cy.get(this.emailLogin).type(email);    
+        cy.get(this.emailLogin).clear()
+        cy.get(this.emailLogin).type("ganso@gmail.com");    
     }
     preencherSenha(senha){
-        cy.get(this.senhaLogin).type(senha);
+        cy.get(this.senhaLogin).clear()
+        cy.get(this.senhaLogin).type("12345678");
     }
     clicarEntrar(){
         cy.get(this.botãoEntrar).click();
 
+}}
 
+//acessar tela de perfil
+class acessPerfil{
+
+    //elementos
+    botãoOpções = '.sc-bjUoiL'
+    botãoPerfil = ":nth-child(3) > .sc-crXcEl > .sc-hHLeRK"
+
+    //Ações     
+    clicarOpções(){
+        cy.get(this.botãoOpções).click();
+    } 
+    clicarPerfil(){
+            cy.get(this.botãoPerfil).click();    
 
 }}
 
+
 export var login = new newLogin();
+export var Perfil = new acessPerfil();
--- cypress/support/steps/perfil.steps.js
+++ cypress/support/steps/perfil.steps.js
@@ -1,10 +1,16 @@
 import {login} from "../pages/perfilPage.po"
+import {Perfil} from "../pages/perfilPage.po"
 
+
+//Acessar tela de perfil para atualizar informações de usuario
 Given("acessei a tela de perfil", () => {
-  
+   //Fazer login
    login.telaLogin();
    login.preencherEmail();
-   
-
+   login.preencherSenha();
+   login.clicarEntrar();
+   //Acessar tela de Perfil
+   Perfil.clicarOpções();
+   Perfil.clicarPerfil();
   }
-);
+);  
