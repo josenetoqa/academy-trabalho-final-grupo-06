fa23e7bef3d235215d9ef41b112e431a15affb08
Romerito <romeritom@hotmail.com>
2022-05-28 20:26:19 -0300

    Karate Feature de Login

---
+++ karate-config.js
@@ -0,0 +1,12 @@
+function setup() {
+    
+   
+    var config = {
+        baseUrl: "https://lista-compras-api.herokuapp.com/api/v1/auth/login",
+        usuarioPadrao: {"email": "romerito@raro.com","password": "123"},
+        
+
+    }
+   
+    return config;
+}
