47a3e0fa66d07c6d6be6d597cd20aff165f7fd10
Amanda  Rodrigues <99908337+aar-amanda@users.noreply.github.com>
2022-05-28 16:20:51 -0300

    arquivos de feature e criarPage por erros no git

---
+++ cypress/support/pages/criarPage.po.js
@@ -0,0 +1,53 @@
+class newUser{
+
+    nome = "#name"
+    email = "#email"
+    senha = "#senha"
+    confirmarSenha = "#confirmarSenha"
+    btnregistrese = '.sc-evZas'
+    btnregistrar = '.sc-kgUAyh'
+
+    telaPrincipal() {
+        cy.visit("https://academy-lembra-compras.herokuapp.com/");
+    }
+
+    registreSe(){
+        cy.get(this.btnregistrese).click();
+    }
+    
+    atribuirNome(nome) {
+        cy.get(this.nome).type(nome);
+    }
+
+    atribuirEmail(email) {
+        cy.get(this.email).type(email);
+    }
+
+    atribuirSenha(senha) {
+        cy.get(this.Senha).type(senha);
+    }
+
+    atribuirConfirmarSenha(confirmarSenha) {
+        cy.get(this.ConfirmarSenha).type(confirmarSenha);
+    }
+
+    icone() {
+        cy.get(this.btnregistrar).click();
+    }
+
+    mensagemDeSucesso(mensagem) {
+        cy.contains(mensagem).should("be.visible");
+    }
+
+    mensagemDeErro(mensagem) {
+        cy.contains(mensagem).should("be.visible");
+    }
+
+    preencherFormulario(nome, email, senha, confirmarSenha) {
+        this.nome(nome);
+        this.email(email);
+        this.senha(senha);
+        this.confirmarSenha(confirmarSenha);
+    }}
+
+export var criarPage = new CriarPage();
