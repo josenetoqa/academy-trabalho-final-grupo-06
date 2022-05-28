a0ed4d6fbe0b61f2867cff1b23e5679b2313633c
Amanda  Rodrigues <99908337+aar-amanda@users.noreply.github.com>
2022-05-28 18:16:35 -0300

    implementação karate config

---
+++ karate/karate-config.js
@@ -0,0 +1,13 @@
+function setup() {
+    var config = {
+        baseUrl: "https://https://lista-compras-api.herokuapp.com/api/v1"
+    }
+
+    return config;
+}
+
+function setup() {
+    karate.configure("headers", {
+        secret: "ak109sa1kj-amsasuil1"
+    })
+}
