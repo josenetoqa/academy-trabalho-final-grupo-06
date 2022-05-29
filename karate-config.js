00748fdcbfbe2ad111b01028f113a37c9825c071
Jose Duarte de Abreu Neto <82482616+joseneto-dev@users.noreply.github.com>
2022-05-29 19:01:01 -0300

    karate 1.5

---
+++ karate-config.js
@@ -0,0 +1,9 @@
+function setup() {
+    var config = {
+        baseUrl: "https://lista-compras-api.herokuapp.com/api/v1",
+        token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpZCI6IjNkYzUwYmY4LTI0NTEtNDEyZS04ZTVjLWQwYWNkNzQxOGJlYiIsImlzX2FkbWluIjpmYWxzZSwiZW1haWwiOiJqb3NlcGFuZGlhNDJAZ21haWwuY29tIiwiZGF0ZUNyZWF0ZWQiOjE2NTM4NTY4MzcxMDAsImlzc3VlZCI6MTY1Mzg1NjgzNzEwMCwiZXhwaXJlcyI6MTY1Mzg1NzczNzEwMH0.IPf60WBN96Q3h3jNia-9FyzB81-5HEbN1OIKbIXJsJ2Xkx1WKL31d7_fHhNooDXLTvTZ7aIj3Cs4z03OA3hUgQ"
+    }
+    
+
+    return config;
+}
